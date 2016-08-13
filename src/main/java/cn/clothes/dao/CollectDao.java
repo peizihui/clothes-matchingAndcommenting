71b782cfe98c2960675b76b8c5be544e776554d0
clq <867359230@qq.com>
2016-08-13 22:38:13 +0800

    收藏操作

--- src/main/java/cn/clothes/dao/CollectDao.java
+++ src/main/java/cn/clothes/dao/CollectDao.java
@@ -1,5 +1,8 @@
 package cn.clothes.dao;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
 import cn.clothes.entity.Collect;
@@ -24,4 +27,13 @@ public interface CollectDao {
 	 * @return
 	 */
 	int deleteCollect(Long clothingId);
+	
+	/**
+	 * 根据用户id查询出收藏作品
+	 * @param userId
+	 * @param offset
+	 * @param limit
+	 * @return
+	 */
+	List<Collect> queryCollectForPage(@Param("userId")Long userId, @Param("offset") int offset, @Param("limit") int limit);
 }
