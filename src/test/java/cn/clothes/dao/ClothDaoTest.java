9d513b0f32895bbd16bf327413efd975f47611a4
clq <867359230@qq.com>
2016-08-13 16:04:43 +0800

    测试用例

--- src/test/java/cn/clothes/dao/ClothDaoTest.java
+++ src/test/java/cn/clothes/dao/ClothDaoTest.java
@@ -1,11 +1,15 @@
 package cn.clothes.dao;
 
+import java.util.List;
+
 import javax.annotation.Resource;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.alibaba.fastjson.JSON;
 
 import cn.clothes.entity.Clothes;
 
@@ -28,5 +32,11 @@ public class ClothDaoTest {
 	public void selectUser() {
 		Clothes queryCloth = dao.queryCloth(1l);
 		System.out.println(queryCloth.getClothIcon());
+	}
+	
+	@Test
+	public void selectClothForPage() {
+		List<Clothes> queryForPage = dao.queryForPage(1, 2);
+		System.out.println(JSON.toJSONString(queryForPage));
 	}
 }
