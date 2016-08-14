07e43581e79b6d692a52c96a5387c1da1da7a148
clq <867359230@qq.com>
2016-08-14 17:44:19 +0800

    文档

--- src/main/java/cn/clothes/service/Impl/ClothServiceImpl.java
+++ src/main/java/cn/clothes/service/Impl/ClothServiceImpl.java
@@ -3,11 +3,13 @@ package cn.clothes.service.Impl;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import cn.clothes.dao.ClothDao;
 import cn.clothes.entity.Clothes;
 import cn.clothes.service.ClothService;
 
+@Service
 public class ClothServiceImpl implements ClothService{
 	
 	@Autowired
