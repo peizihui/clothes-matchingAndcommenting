0a987f3e442c04279d04a66e27dd4d6fb964d856
clq <867359230@qq.com>
2016-08-14 23:38:58 +0800

    控制器

---
+++ src/main/java/cn/clothes/controller/ClothController.java
@@ -0,0 +1,53 @@
+package cn.clothes.controller;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import cn.clothes.service.ClothService;
+/**
+ * 服装api
+ * @author clq
+ *
+ */
+@Controller
+@RequestMapping("/cloth")
+public class ClothController {
+	
+	@Autowired
+	private ClothService service;
+	
+	@RequestMapping(value="/upload", method = RequestMethod.POST
+            ,produces = {"application/json; charset=UTF-8"})
+    @ResponseBody
+	public Object uploadImage(@RequestParam("name") String name, @RequestPart MultipartFile file, String content) {
+		if (!file.isEmpty()) {
+            try {
+                byte[] bytes = file.getBytes();
+                BufferedOutputStream stream =
+                        new BufferedOutputStream(new FileOutputStream(new File(name + "-uploaded")));
+                stream.write(bytes);
+                stream.close();
+                return "You successfully uploaded " + name + " into " + name + "-uploaded !";
+            } catch (Exception e) {
+                return "You failed to upload " + name + " => " + e.getMessage();
+            }
+        } else {
+            return "You failed to upload " + name + " because the file was empty.";
+        }
+	}
+	
+	@RequestMapping(value="/index", method = RequestMethod.GET)
+	public String index() {
+		return "jsp/upload";
+	}
+}
